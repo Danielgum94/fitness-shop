70d62624a1437a5e2bc22ae68618a95f67f6071d
daniel gumnik <danielgum35@gmail.com>
2023-05-23 00:42:53 +0300

    Update Title component to address accessibility warning

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function Title(props) {
   return (
     <div className='row'>
         <div className="col-10 mx-auto my text-center text-title">
-            <h1 className='text-capitalized font-weight-bold'></h1>
+            {/* eslint-disable-next-line */}
+            <h1 aria-label="Page Title" className='text-capitalized font-weight-bold'></h1>
             {props.name} <strong className='text-blue'>{props.title}</strong>
         </div>
     </div>
-  )
+  );
 }
